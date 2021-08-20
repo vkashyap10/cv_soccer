ab37c8e675dfa5bdcc6a5da19ae8d64bd3b46564
Vishal Kashyap <vkashyap1007@gmail.com>
2021-08-20 19:30:08 +0530

    modified to calculate gradient within given window

--- return_gradient.py
+++ return_gradient.py
@@ -4,7 +4,7 @@ from numpy import linalg as LA
 
 # function to return gradient in an image using sobel operator
 
-def return_gradient(imCrop):
+def return_gradient(imCrop,sind_x,eind_x,sind_y,eind_y):
     # convert image to greyscale
     grayscale_im = cv2.cvtColor(imCrop, cv2.COLOR_BGR2GRAY)
 
@@ -12,13 +12,13 @@ def return_gradient(imCrop):
     gradient_im_x = np.zeros(grayscale_im.shape)
     gradient_im_y = np.zeros(grayscale_im.shape)
     # apply Sobel operator
-    for i in range(1,grayscale_im.shape[0]-1):
-        for j in range(1,grayscale_im.shape[1]-1):
+    for i in range(sind_x,eind_x):
+        for j in range(sind_y,eind_y):
             gradient_im_x[i,j] = (2/8*grayscale_im[i,j+1] - 2/8*grayscale_im[i,j-1]) 
             + 1/8*grayscale_im[i+1,j+1]+ 1/8*grayscale_im[i-1,j+1] - (1/8*grayscale_im[i-1,j-1]+ 1/8*grayscale_im[i+1,j-1] )
 
-    for i in range(1,grayscale_im.shape[0]-1):
-        for j in range(1,grayscale_im.shape[1]-1):
+    for i in range(sind_x,eind_x):
+        for j in range(sind_y,eind_y):
             gradient_im_y[i,j] = (2/8*grayscale_im[i+1,j] - 2/8*grayscale_im[i-1,j]) 
             + 1/8*grayscale_im[i+1,j+1]+ 1/8*grayscale_im[i+1,j-1] - (1/8*grayscale_im[i-1,j-1]+ 1/8*grayscale_im[i-1,j+1] )
 
