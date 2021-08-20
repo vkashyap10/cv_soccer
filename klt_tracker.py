34cce9bfce5a28ac036af9235b0608cce68d5548
Vishal Kashyap <vkashyap1007@gmail.com>
2021-08-20 19:28:42 +0530

    does not work well

--- klt_tracker.py
+++ klt_tracker.py
@@ -11,29 +11,49 @@ def klt_tracker(frame0,frame1,gradx,grady,f2trackcord):
 	frame0 = cv2.cvtColor(frame0, cv2.COLOR_BGR2GRAY)
 	frame1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
 
-	frame2track0 = frame0[int(f2trackcord[0]-1):int(f2trackcord[0]+2),int(f2trackcord[1]-1):int(f2trackcord[1]+2)]
-	gradx_frame = gradx[int(f2trackcord[0]-1):int(f2trackcord[0]+2),int(f2trackcord[1]-1):int(f2trackcord[1]+2)]
-	grady_frame = grady[int(f2trackcord[0]-1):int(f2trackcord[0]+2),int(f2trackcord[1]-1):int(f2trackcord[1]+2)]
+	window = 20
+	frame2track0 = frame0[int(f2trackcord[0]-window):int(f2trackcord[0]+window+1),int(f2trackcord[1]-window):int(f2trackcord[1]+window+1)]
+	gradx_frame = gradx[int(f2trackcord[0]-window):int(f2trackcord[0]+window+1),int(f2trackcord[1]-window):int(f2trackcord[1]+window+1)]
+	grady_frame = grady[int(f2trackcord[0]-window):int(f2trackcord[0]+window+1),int(f2trackcord[1]-window):int(f2trackcord[1]+window+1)]
 
 	# temporal difference
-	frame2track1 = frame1[int(f2trackcord[0]-1):int(f2trackcord[0]+2),int(f2trackcord[1]-1):int(f2trackcord[1]+2)]
+	frame2track1 = frame1[int(f2trackcord[0]-window):int(f2trackcord[0]+window+1),int(f2trackcord[1]-window):int(f2trackcord[1]+window+1)]
 	It = frame2track1 - frame2track0
 
 	Atb = np.zeros((2,1))
 
 	# moment matrix
 	M = np.zeros((2,2))
-	for i in range(3):
-		for j in range(3):
+	for i in range(gradx_frame.shape[0]):
+		for j in range(gradx_frame.shape[1]):
 		    M[0,0] = M[0,0] + (gradx_frame[i,j])**2
 		    M[1,1] = M[1,1] + (grady_frame[i,j])**2
 		    M[1,0] = M[1,0] + grady_frame[i,j]*gradx_frame[i,j]
 		    M[0,1] = M[0,1] + grady_frame[i,j]*gradx_frame[i,j]
 
-		    print()
 		    Atb[0,0] = Atb[0,0] + It[i,j]*gradx_frame[i,j]
 		    Atb[1,0] = Atb[1,0] + It[i,j]*grady_frame[i,j]
 
 	vel = np.matmul(np.linalg.inv(M),-Atb)
 
-	return vel
+	# given velocity find feature in new frame
+	# navie : assume tracking is optimal
+	f2trackcord_new = f2trackcord + (vel).astype(int)
+
+	# find best new match
+	# best_match = f2trackcord_new
+	# ssd_min = 1e9
+	# for i in range(20):
+	# 	for j in range(20):
+	# 		newc_i = f2trackcord_new[0] - i + 10
+	# 		newc_j = f2trackcord_new[1] - j + 10
+	# 		new_window = frame1[int(newc_i-window):int(newc_i+window+1),int(newc_j-window):int(newc_j+window+1)]
+	# 		ssd = np.sum((new_window- frame2track0)**2)
+	# 		if(ssd < ssd_min):
+	# 			ssd_min = ssd
+	# 			best_match[0] = newc_i
+	# 			best_match[1] = newc_j
+
+	print("best_match",f2trackcord_new)
+
+	return f2trackcord_new,(vel).astype(int)
